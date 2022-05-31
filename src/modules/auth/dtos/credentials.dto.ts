8c172841abbd4f8e5a3d8fe03771bb5ac5a20c9c
John Menaker <menaker.john@outlook.com>
2022-05-31 16:53:08 -0500

    Added credentials validation

--- src/modules/auth/dtos/credentials.dto.ts
+++ src/modules/auth/dtos/credentials.dto.ts
@@ -1,9 +1,16 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import * as mongoose from 'mongoose';
 import { Role } from 'src/modules/roles';
 
 export class Credentials {
   _id: mongoose.Types.ObjectId;
+  
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
   roles: Role[];
   profile: mongoose.Types.ObjectId;
