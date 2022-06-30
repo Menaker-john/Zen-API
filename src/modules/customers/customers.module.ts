043eaa8f409accf91f509be852a71044e1dbf580
John Menaker <menaker.john@outlook.com>
2022-06-30 17:58:16 -0500

    Started Customer Schema and module.

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -1,10 +1,12 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { RepositoryModule } from '../repository';
+import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
 
 @Module({
   imports: [RepositoryModule],
-  controllers: [],
-  providers: [],
+  controllers: [CustomersController],
+  providers: [CustomersService],
   exports: [],
 })
 export class CustomersModule {}
--- src/schema/customer.schema.ts
+++ src/schema/customer.schema.ts
@@ -1,9 +1,38 @@
 import * as mongoose from 'mongoose';
 import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ContactInfo } from './contact-info.schema';
 
 export type CustomerDocument = Customer & mongoose.Document;
 
+enum Status {
+  Prospective = 'Prospective',
+  Active = 'Active',
+  Deactivated = 'Deactivated',
+  Canceled = 'Canceled',
+}
+
 @Schema()
-export class Customer {}
+export class Customer {
+  @Prop()
+  _id: mongoose.Types.ObjectId;
+
+  @Prop(
+    raw({
+      first: { type: String },
+      middle: { type: String },
+      last: { type: String },
+    }),
+  )
+
+  @Prop()
+  contactInfo: ContactInfo;
+
+  @Prop({type: String, enum: Status})
+  status: Status;
+
+  @Prop({type: mongoose.Types.ObjectId, ref: 'Profile'})
+  accountOwner: mongoose.Types.ObjectId
+
+}
 
 export const CustomerSchema = SchemaFactory.createForClass(Customer);
